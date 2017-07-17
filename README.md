87049b5c475b8684ea5110b5b99e73a29b25a59f
Paula Ponce <paula.poncez@usach.cl>
2017-07-17 01:06:26 -0400

    readme y correcciones

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Convertidor de celsius a fahrenheit
+# Lección 14 - HTML
+## Convertidor de Unidades de Temperatura
+
+Interfaz de un convertidor de unidades de temperatura: Fahrenheit, Celsius, Kelvin y Réaumur.
+
+* Se crea una tabla con inputs para cada unidad de temperatura
+* Para hacer la conversión se debe ingresar la temperatura en el input correspondiente según su unidad
+* Al hacer click en el botón calcular, devuelve el resultado en los inputs correspondientes a las demás unidades de temperatura
+* Se agrega botón borrar que limpia los campos para realizar una nueva conversión
+
+[gh-pages](https://paulaponce.github.io/conversor-temperatura/)
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,26 +1,26 @@
 var fahrenheit = document.getElementById("fahrenheit");
-var celcius = document.getElementById("celcius");
+var celsius = document.getElementById("celsius");
 var kelvin = document.getElementById("kelvin");
 var reaumur = document.getElementById("reaumur");
 
 function convertirTemperatura(){
-	if(fahrenheit.value != "" && celcius.value != "" && kelvin.value != "" && reaumur.value != ""){
+	if(fahrenheit.value != "" && celsius.value != "" && kelvin.value != "" && reaumur.value != ""){
 		alert("Presione el botón 'Borrar' para realizar otra conversión.");
 	}else if(fahrenheit.value != ""){
-		celcius.value = fahrenheit.value - 32 * 5/9;
+		celsius.value = fahrenheit.value - 32 * 5/9;
 		kelvin.value = fahrenheit.value - 32 * 5/9 + 273.15;
 		reaumur.value = fahrenheit.value - 32 * 4/9;
-	}else if(celcius.value != ""){
-		fahrenheit.value = celcius.value * (9/5 + 32);
-		kelvin.value = celcius.value * 1 + 273.15;
-		reaumur.value = celcius.value * 4/5;
+	}else if(celsius.value != ""){
+		fahrenheit.value = celsius.value * (9/5 + 32);
+		kelvin.value = celsius.value * 1 + 273.15;
+		reaumur.value = celsius.value * 4/5;
 	}else if(kelvin.value != ""){
 		fahrenheit.value = kelvin.value - 273.15 * 9/5 + 32;
-		celcius.value = kelvin.value * 1 - 273.15;
+		celsius.value = kelvin.value * 1 - 273.15;
 		reaumur.value = kelvin.value - 273.15 * 4/5;
 	}else if(reaumur.value != ""){
 		fahrenheit.value = reaumur.value * 9/4 + 32;
-		celcius.value = reaumur.value * 5/4;
+		celsius.value = reaumur.value * 5/4;
 		kelvin.value = reaumur.value * 5/4 + 273.15;
 	}else{
 		alert("Ingrese temperatura en uno de los campos.");
@@ -29,7 +29,7 @@ function convertirTemperatura(){
 
 function limpiarCampos(){
 	fahrenheit.value = "";
-	celcius.value = "";
+	celsius.value = "";
 	kelvin.value = "";
 	reaumur.value = "";
 }
