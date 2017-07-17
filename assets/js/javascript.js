31329e7df0aa6785f952045f1b74ac1c03173860
Paula Ponce <paula.poncez@usach.cl>
2017-07-17 00:07:08 -0400

    función que convierte temperatura ingresada en input a otras escalas de temperatura, se añaden kelvin y réaumur

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,35 @@
-function convertirCelsius(celsius){
-	var celsius = document.getElementById("celsius").value;
-	//var fahr = document.getElementById("fahrenheit");
-	var resultado = (1.8 * celsius) + 32; 
-	alert(celsius + " °C = " + resultado + "°F");
-	document.getElementById("fahrenheit")=resultado;
+var fahrenheit = document.getElementById("fahrenheit");
+var celcius = document.getElementById("celcius");
+var kelvin = document.getElementById("kelvin");
+var reaumur = document.getElementById("reaumur");
+
+function convertirTemperatura(){
+	if(fahrenheit.value != "" && celcius.value != "" && kelvin.value != "" && reaumur.value != ""){
+		alert("Presione el botón 'Borrar' para realizar otra conversión.");
+	}else if(fahrenheit.value != ""){
+		celcius.value = fahrenheit.value - 32 * 5/9;
+		kelvin.value = fahrenheit.value - 32 * 5/9 + 273.15;
+		reaumur.value = fahrenheit.value - 32 * 4/9;
+	}else if(celcius.value != ""){
+		fahrenheit.value = celcius.value * (9/5 + 32);
+		kelvin.value = celcius.value * 1 + 273.15;
+		reaumur.value = celcius.value * 4/5;
+	}else if(kelvin.value != ""){
+		fahrenheit.value = kelvin.value - 273.15 * 9/5 + 32;
+		celcius.value = kelvin.value * 1 - 273.15;
+		reaumur.value = kelvin.value - 273.15 * 4/5;
+	}else if(reaumur.value != ""){
+		fahrenheit.value = reaumur.value * 9/4 + 32;
+		celcius.value = reaumur.value * 5/4;
+		kelvin.value = reaumur.value * 5/4 + 273.15;
+	}else{
+		alert("Ingrese temperatura en uno de los campos.");
+	}
+}
+
+function limpiarCampos(){
+	fahrenheit.value = "";
+	celcius.value = "";
+	kelvin.value = "";
+	reaumur.value = "";
 }
